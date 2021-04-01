7ec90ea2c9161234309f44af9c93d72c14234d5d
Simon Jefferies <simon@olivegame.studio>
2021-04-01 09:37:30 +0100

    Imported.

---
+++ Sleep/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeMilliseconds = int.Parse(args[0]);
+            Thread.Sleep(timeMilliseconds);
+        }
+    }
+}
